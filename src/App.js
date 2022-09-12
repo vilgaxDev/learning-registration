8656a13c829a82a873bd63ddfaf1882f24f89eb2
vilgax <ken.vilgax@gmail.com>
2022-09-12 17:33:12 +0300

    setup

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div className="App">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+        <p>
+          Developer training <code>src/App.js</code>
         </p>
         <a
           className="App-link"
